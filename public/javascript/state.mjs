55f3e555407bf11531ed550ba484d5345b638255
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2021-08-13 12:51:29 +0300

    Client JavaScript: the JavaScript Standard Style is used

--- public/javascript/state.mjs
+++ public/javascript/state.mjs
@@ -1,6 +1,7 @@
+/* eslint-env browser */
 // //////////////////////////////////////////////////
-//	Piece for smartphone orchestra
-//			by Tassos Tsesmetzis
+//  Piece for smartphone orchestra
+//      by Tassos Tsesmetzis
 //
 // This file defines the object 'State'. It is a
 // hybrid of radio button and check box object.
@@ -11,24 +12,23 @@
 // The method 'change' changes state (presses - deselects a button).
 // When the argument is different than current state, pass it to current state (press radio button).
 // When the argument is the same as current state, change state to neutral (deselect a button)
-import { EventDispatcher } from './eventDispatchers.mjs';
+import { EventDispatcher } from './eventDispatchers.mjs'
 
 export default class State {
-    constructor () {
-	this.current = -1;
-	this.all = Array.from(arguments);
-	this.hasChanged = false;
-	this.listeners = new EventDispatcher(this);
+  constructor () {
+    this.current = -1
+    this.all = Array.from(arguments)
+    this.hasChanged = false
+    this.listeners = new EventDispatcher(this)
+  }
+
+  change (anInteger) {
+    if (this.all.includes(anInteger)) {
+      this.current = this.current === anInteger ? (this.hasChanged = false, -1) : (this.hasChanged = true, anInteger)
+
+      this.listeners.notify(anInteger)
+    } else {
+      throw new Error('Argument is not a valid state.')
     }
-
-    change (anInteger) {
-	if (this.all.includes(anInteger)) {
-	    this.current = this.current === anInteger ? (this.hasChanged = false, -1) : (this.hasChanged = true, anInteger);
-
-	    this.listeners.notify(anInteger);
-	} else {
-	    throw new Error('Argument is not a valid state.');
-
-	}
-    }
+  }
 }
