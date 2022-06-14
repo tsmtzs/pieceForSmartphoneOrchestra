8e8c7034d39e3100e10832c802b27be80c58dfdb
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2022-06-14 19:05:26 +0300

    Client: Changes in the page index.html
    * src/stylesheet.css:
    	- Selectors 'main h2.otherH2', 'button.switch', 'button.switch span'
    		'label.switch' and aria deleted.
    
    * src/javascript/index.js:
    	- All constants and imported functions and objects deleted.
    
    * src/views/index.html:
    	- Paragraphs where rewritten.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -5,22 +5,6 @@
 //
 // Main JavaScript file for views/index.html
 // //////////////////////////////////////////////////
-import {
-  extendBtns,
-  connectSensor,
-  logErrorAfterElement,
-  getSensorActivateListenerForElement,
-  revealElement,
-  setBackgroundColorAndBorderToButtons,
-  initSound,
-  attachListenerToState,
-  createSoundObjectsForState,
-  createReferenceSoundAndAddPointerdownListener,
-  addReadingListenerToSensor
-} from './functions.mjs'
-
-import { State } from './state.mjs'
-
 // Register the ServiveWorker
 // from https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register
 if ('serviceWorker' in navigator) {
@@ -30,26 +14,3 @@ if ('serviceWorker' in navigator) {
 } else {
   console.log('Service workers are not supported.')
 }
-
-// const state = new State(0)
-// const instrButton = document.querySelector('#instrument')
-// const refButton = document.querySelector('#refTone')
-
-// extendBtns([instrButton], state)
-
-// const body = document.querySelector('body')
-// const bar = document.querySelector('#bar')
-// const position = document.querySelector('#barPoint')
-// const buttonSection = document.querySelector('#buttons')
-// const sensor = new window.AbsoluteOrientationSensor()
-
-// connectSensor(sensor)
-//   .then(getSensorActivateListenerForElement(bar))
-//   .then(setBackgroundColorAndBorderToButtons([instrButton, refButton]))
-//   .then(revealElement(buttonSection))
-//   .then(initSound)
-//   .then(attachListenerToState(state, [instrButton]))
-//   .then(createSoundObjectsForState(state))
-//   .then(createReferenceSoundAndAddPointerdownListener)
-//   .then(addReadingListenerToSensor(sensor, bar, position))
-//   .catch(logErrorAfterElement(body))
