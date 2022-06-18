9354f1daf52f1062973ecd6d1236933086a52113
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2022-06-18 20:08:39 +0300

    CHANGELOG.md updated for version 2.1.0

--- src/javascript/parameters.mjs
+++ src/javascript/parameters.mjs
@@ -8,7 +8,6 @@
 // Sound:
 const BASE_FREQ = 1244.5079348883 // Eflat 6
 const MAX_AMP = 0.9
-const REFTONE_AMP = 0.3
 const FADE_IN = 0.05
 const FADE_OUT = 0.05
 // Button
@@ -23,7 +22,6 @@ const DISPLAY_TOP_VECTOR = [0, 1, 0]
 export {
   BASE_FREQ,
   MAX_AMP,
-  REFTONE_AMP,
   FADE_IN,
   FADE_OUT,
   BTN_COLOR_ON,
