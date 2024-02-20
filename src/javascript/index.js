986682e8fc42a4138daceaf241d58506a12600f7
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2024-02-20 14:10:10 +0200

    Webmanifest: 'icon' key-value pair deleted

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -8,9 +8,9 @@
 // Register the ServiveWorker
 // from https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register(new URL('../serviceWorker.js', import.meta.url), { scope: '/' })
-    .catch(error => console.log('Service worker registration failed:', error))
+  // navigator.serviceWorker
+  //   .register(new URL('../serviceWorker.js', import.meta.url), { scope: '/' })
+  //   .catch(error => console.log('Service worker registration failed:', error))
 } else {
   console.log('Service workers are not supported.')
 }
