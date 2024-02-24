6a42e61327809f8914ba6c803655abfb36121a31
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2024-02-24 08:48:06 +0200

    Plugin @vitejs/plugon-basic-ssl added
    * astro.config.mjs:
    	- Function 'basicSsl' imported.
    	- A 'vite' key added to the parameters of 'defineConfig'.
    * package.json:
    	- The 'config' key removed. Changes in 'scripts.preview' and 'scripts.dev'.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config'
 import serviceWorker from 'astrojs-service-worker'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     serviceWorker()
-  ]
+  ],
+  vite: {
+    plugins: [basicSsl()]
+  }
 })
