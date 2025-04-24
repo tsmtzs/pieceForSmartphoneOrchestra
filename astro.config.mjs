ec2223ae0eac9cdb0d730cf61efcb4210dceae1b
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2025-04-24 19:46:56 +0300

    package.json: changes in scripts 'make-png-icons', 'build', 'pwa-svg-icon'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,10 @@
 import { defineConfig } from 'astro/config'
 import serviceWorker from 'astrojs-service-worker'
-import webmanifest from 'astro-webmanifest'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-import info from './package.json'
 
 export default defineConfig({
   integrations: [
-    serviceWorker(),
-    webmanifest({
-      name: 'Piece for Smartphone Orchestra',
-      short_name: 'Smartphone Orchestra',
-      description: 'A sound piece for smartphones.',
-      display: 'standalone',
-      background_color: `#${info.config.colors.background}`,
-      theme_color: `#${info.config.colors.dark}`,
-      start_url: '/',
-      orientation: 'portrait'
-    })
+    serviceWorker()
   ],
   vite: {
     plugins: [basicSsl()]
