861a5ab0961965d62dc237991a8417833810d9b7
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2025-04-24 17:07:04 +0300

    astro.congig.mjs: package astro-webmanifest is used to generate the webmanifest file of the PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import { defineConfig } from 'astro/config'
 import serviceWorker from 'astrojs-service-worker'
+import webmanifest from 'astro-webmanifest'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import info from './package.json'
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
-    serviceWorker()
+    serviceWorker(),
+    webmanifest({
+      name: 'Piece for Smartphone Orchestra',
+      short_name: 'Smartphone Orchestra',
+      description: 'A sound piece for smartphones.',
+      display: 'standalone',
+      background_color: `#${info.config.colors.background}`,
+      theme_color: `#${info.config.colors.dark}`,
+      start_url: '/',
+      orientation: 'portrait'
+    })
   ],
   vite: {
     plugins: [basicSsl()]
