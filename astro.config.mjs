fa60149fd2835a94a3d09480377a84994427ad05
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2025-04-25 19:34:28 +0300

    Additions in astro.config.mjs and public/smartphoneOrchestra.webmanifest
    * astro.config.mjs
    	ADDED
    		- Argument object of 'defineConfig': Key 'site'.
    * public/smartphoneOrchestra.webmanifest
    	REMOVED
    		- 'icon' entry with size 32x32.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import serviceWorker from 'astrojs-service-worker'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+  site: 'https://tsmtzs.github.io/pieceForSmartphoneOrchestra/',
   integrations: [
     serviceWorker()
   ],
